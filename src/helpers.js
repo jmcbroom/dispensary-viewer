2f405b4609a45d35a1b461fc31b3059f80a63302
Jimmy McBroom <james.mcbroom@gmail.com>
2017-10-23 21:06:07 -0400

    fix it up

---
+++ src/helpers.js
@@ -0,0 +1,14 @@
+const Helpers = {
+
+  /** 
+   * Increments a number by one
+   * @param {number} num - a positive or negative number
+   * @returns {number} - num plus one
+   */
+  example: function(num) {
+    return num + 1;
+  }
+
+}
+
+export default Helpers;
---
+++ src/main.js
@@ -0,0 +1,16 @@
+var mapboxgl = require('mapbox-gl');
+
+import Helpers from './helpers.js';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2l0eW9mZGV0cm9pdCIsImEiOiJjaXZvOWhnM3QwMTQzMnRtdWhyYnk5dTFyIn0.FZMFi0-hvA60KYnI-KivWg';
+
+// draw a map
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v8',
+    center: [-83.091, 42.350],
+    zoom: 9
+})
+
+// execute a function
+console.log(Helpers.example(-3.5));
